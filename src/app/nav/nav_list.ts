ba62c296ef7d40e94812de89e213b7a2271bd46e
TheFizsz <Asianthunder1234@gmail.com>
2018-10-07 17:59:14 -0400

    Finished the dropdown. Landing page is next

--- src/app/nav/nav_list.ts
+++ src/app/nav/nav_list.ts
@@ -3,23 +3,24 @@ import { NavItem } from './nav_object';
 
 //Create a HEROES list from the Hero class and export it
 export const NavList: NavItem[] = [
-  { id: 1, name: 'The Empire' },
-  { id: 2, name: 'Dwarves' },
-  { id: 3, name: 'Kislev' },
-  { id: 4, name: 'Bretonnia' },
-  { id: 5, name: 'High Elves' },
-  { id: 6, name: 'Wood Elves' },
-  { id: 7, name: 'Lizard Men' },
-  { id: 8, name: 'Stormcast' },
-  { id: 9, name: 'Sea Elves' },
-  { id: 10, name: 'Dogs of War' },
-  { id: 11, name: 'Chaos Warriors' },
-  { id: 12, name: 'Chaos Dwarves' },
-  { id: 13, name: 'Beastmen' },
-  { id: 14, name: 'Vampire Counts' },
-  { id: 15, name: 'Tomb Kings' },
-  { id: 16, name: 'Dark Elves' },
-  { id: 17, name: 'Skaven' },
-  { id: 18, name: 'Ogre Kingdoms' },
-  { id: 19, name: 'Orks' }
+  
+  { id: 1, name: 'The Empire', infantry_image: 'https://www.games-workshop.com/resources/catalog/product/920x950/99120202013_StateTroopsNEW01.jpg', missile_image: 'https://www.games-workshop.com/resources/catalog/product/920x950/99120202012_HandgunnersNEW01.jpg', cavalry_image: 'https://www.games-workshop.com/resources/catalog/product/920x950/99120202032_DemigryphKnights01.jpg', hero_image: 'https://www.games-workshop.com/resources/catalog/product/920x950/99120202011_GeneraloftheEmpireNEW01.jpg', monster_image: 'https://www.games-workshop.com/resources/catalog/product/920x950/99120202030_KarlFranzonDeathclawNEW01.jpg', magic_image: 'https://www.games-workshop.com/resources/catalog/product/920x950/99120202036_CollegiateBattleWizards01.jpg'},
+  { id: 2, name: 'Dwarves', infantry_image: 'https://www.games-workshop.com/resources/catalog/product/threeSixty/99120205026_DispossessedIronbreakers2360/01-01.jpg', missile_image: 'https://i.pinimg.com/564x/4e/e8/ee/4ee8eee337ce97d377e827e2fd01463b.jpg', cavalry_image: 'No Cavalry :(', hero_image: 'https://i.pinimg.com/564x/02/c2/ea/02c2ea939c4f33364feb0972b5590a5d.jpg', monster_image: 'No Monsters :(', magic_image: 'https://i.pinimg.com/564x/9c/a1/65/9ca1655193f2a59167e473a3c3173271.jpg'},
+  { id: 4, name: 'Bretonnia', infantry_image: 'https://i.pinimg.com/564x/09/06/fe/0906fe9156314ec4c01810ed3ce0b47b.jpg', missile_image: 'https://i.pinimg.com/564x/43/bd/fd/43bdfd440e523845a2b3550e71e90572.jpg', cavalry_image: 'https://i.pinimg.com/564x/1f/08/14/1f08140f7803418062c52c9a69c57fc3.jpg', hero_image: 'https://i.pinimg.com/564x/95/c9/0e/95c90e9df0a13af216226255a4e0311d.jpg', monster_image: 'https://i.pinimg.com/564x/7b/09/dd/7b09dd6bcbc13aad2e0c02ad7b8d0487.jpg', magic_image: 'https://i.pinimg.com/564x/b0/d5/17/b0d5175f8d3c7f9b8363c5f08548ec29.jpg',},
+  /*{ id: 5, name: 'High Elves', infantry_image: , missile_image: , cavalry_image:, hero_image: , monster_image: , magic_image:},
+  { id: 6, name: 'Wood Elves', infantry_image: , missile_image: , cavalry_image:, hero_image: , monster_image: , magic_image:},
+  { id: 7, name: 'Stormcast', infantry_image: , missile_image: , cavalry_image:, hero_image: , monster_image: , magic_image:},
+  { id: 8, name: 'Lizard Men', infantry_image: , missile_image: , cavalry_image:, hero_image: , monster_image: , magic_image:},
+  { id: 9, name: 'Sea Elves', infantry_image: , missile_image: , cavalry_image:, hero_image: , monster_image: , magic_image:},
+  { id: 10, name: 'Dogs of War', infantry_image: , missile_image: , cavalry_image:, hero_image: , monster_image: , magic_image:},
+  { id: 11, name: 'Chaos Warriors', infantry_image: , missile_image: , cavalry_image:, hero_image: , monster_image: , magic_image:},
+  { id: 12, name: 'Chaos Dwarves', infantry_image: , missile_image: , cavalry_image:, hero_image: , monster_image: , magic_image:},
+  { id: 13, name: 'Beastmen', infantry_image: , missile_image: , cavalry_image:, hero_image: , monster_image: , magic_image:},
+  { id: 14, name: 'Vampire Counts', infantry_image: , missile_image: , cavalry_image:, hero_image: , monster_image: , magic_image:},
+  { id: 15, name: 'Tomb Kings', infantry_image: , missile_image: , cavalry_image:, hero_image: , monster_image: , magic_image:},
+  { id: 16, name: 'Dark Elves', infantry_image: , missile_image: , cavalry_image:, hero_image: , monster_image: , magic_image:},
+  { id: 17, name: 'Skaven', infantry_image: , missile_image: , cavalry_image:, hero_image: , monster_image: , magic_image:},
+  { id: 18, name: 'Ogre Kingdoms', infantry_image: , missile_image: , cavalry_image:, hero_image: , monster_image: , magic_image:},
+  { id: 19, name: 'Orks', infantry_image: , missile_image: , cavalry_image:, hero_image: , monster_image: , magic_image:},*/
+
 ];
--- src/app/nav/nav_object.ts
+++ src/app/nav/nav_object.ts
@@ -2,4 +2,10 @@
 export class NavItem {
     id: number;
     name: string;
+    infantry_image: string;
+    missile_image: string;
+    cavalry_image: string;
+    hero_image: string;
+    monster_image: string;
+    magic_image: string; 
   }
